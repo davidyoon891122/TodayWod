877af9a6fe05eaccc7026271f5fc54073c5af28d
David Yoon <qqpo12@naver.com>
2024-09-07 09:32:59 +0900

    Initial Commit

---
+++ TodayWod/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodayWod
+//
+//  Created by Jiwon Yoon on 9/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodayWod/TodayWodApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayWodApp.swift
+//  TodayWod
+//
+//  Created by Jiwon Yoon on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TodayWodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
