d1c5194a334ae1b52763b78ab979e8da64ef951c
David Yoon <davidyoon@hanwha.com>
2024-09-13 10:37:19 +0900

    [TOW-19] 홈 디폴트 설정 화면 개발
    * 홈 디폴트 설정 화면 개발

--- TodayWod/Features/Home/HomeFeature.swift
+++ TodayWod/Features/Home/HomeFeature.swift
@@ -11,8 +11,11 @@ import ComposableArchitecture
 @Reducer
 struct HomeFeature {
 
+    @ObservableState
     struct State: Equatable {
-
+        let title: String = "운동 루틴을 만들어보세요"
+        let subTitle: String = "월요일부터 토요일까지 \n자동으로 새로운 운동 프로그램을 만들어요"
+        let buttonTitle: String = "새로운 운동 만들기"
     }
 
     enum Action {
@@ -37,8 +40,43 @@ struct HomeView: View {
 
     var body: some View {
         VStack {
-            Text("Home")
+            Image(systemName: "person.circle")
+                .resizable()
+                .frame(width: 120.0, height: 120.0)
+                .padding(.top, 120.0)
+            
+            Text(store.title)
+                .bold()
+                .font(.system(size: 20.0))
+                .padding(.top, 30.0)
+                
+            Text(store.subTitle)
+                .font(.system(size: 16.0))
+                .padding(.top, 10.0)
+                .multilineTextAlignment(.center)
+            
+            Button(action: {
+                
+            }, label: {
+                Text(store.buttonTitle)
+                    .bold()
+                    .font(.system(size: 16.0))
+            })
+            .foregroundStyle(.white)
+            .frame(maxWidth: .infinity, minHeight: 56.0)
+            .background(.cyan)
+            .clipShape(.rect(cornerRadius: 300.0))
+            .padding(.horizontal, 38.0)
+            .padding(.top, 40.0)
+            
+            Spacer()
         }
     }
     
+}
+
+#Preview {
+    HomeView(store: Store(initialState: HomeFeature.State()) {
+        HomeFeature()
+    })
 }
