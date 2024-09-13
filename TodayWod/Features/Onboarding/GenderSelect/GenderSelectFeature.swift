efc409dfdb1e9599f8237f43b72a4cc704786157
David Yoon <davidyoon@hanwha.com>
2024-09-13 16:28:29 +0900

    [TOW-18] OnboardingUserInfoModel
    * OnboardingUserInfoModel 추가
    * SwitchStore 연결 부 변경
    * UserDefaultsManager 함수 추가

--- TodayWod/Features/Onboarding/GenderSelect/GenderSelectFeature.swift
+++ TodayWod/Features/Onboarding/GenderSelect/GenderSelectFeature.swift
@@ -8,11 +8,6 @@
 import Foundation
 import ComposableArchitecture
 
-enum GenderType {
-    case man
-    case woman
-}
-
 @Reducer
 struct GenderSelectFeature {
     
@@ -22,6 +17,7 @@ struct GenderSelectFeature {
         let subTitle: String = "성별을 선택해주세요."
         var gender: GenderType? = nil
         var path = StackState<NicknameFeature.State>()
+        var onboardingUserModel: OnboardingUserInfoModel = .init()
     }
     
     enum Action {
@@ -37,6 +33,7 @@ struct GenderSelectFeature {
             switch action {
             case let .setGender(genderType):
                 state.gender = genderType
+                state.onboardingUserModel.gender = genderType
                 let generator = UIImpactFeedbackGenerator(style: .medium)
                 generator.prepare()
                 generator.impactOccurred()
@@ -67,56 +64,57 @@ struct GenderSelectView: View {
     @Perception.Bindable var store: StoreOf<GenderSelectFeature>
     
     var body: some View {
-        NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
-            
-            VStack {
-                HStack {
-                    Text(store.title)
-                        .bold()
-                        .font(.system(size: 24.0))
-                    Spacer()
-                }
-                .padding(.top, 58.0)
-                .padding(.horizontal, 20.0)
-                HStack {
-                    Text(store.subTitle)
-                        .font(.system(size: 20.0))
-                        .foregroundStyle(.grey80)
-                    Spacer()
-                }
-                .padding(.top, 16.0)
-                .padding(.horizontal, 20.0)
-                
-                HStack {
-                    Button(action: {
-                        // TODO: set genderType to man
-                        store.send(.setGender(.man))
-                    }, label: {
-                        Images.genderMan.swiftUIImage
-                            .resizable()
-                            .frame(width: 160, height: 160)
-                            .clipShape(.circle)
-                            .opacity(store.gender == .man ? 1.0 : 0.6)
-                    })
+        WithPerceptionTracking {
+            NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
+                VStack {
+                    HStack {
+                        Text(store.title)
+                            .bold()
+                            .font(.system(size: 24.0))
+                        Spacer()
+                    }
+                    .padding(.top, 58.0)
+                    .padding(.horizontal, 20.0)
+                    HStack {
+                        Text(store.subTitle)
+                            .font(.system(size: 20.0))
+                            .foregroundStyle(.grey80)
+                        Spacer()
+                    }
+                    .padding(.top, 16.0)
+                    .padding(.horizontal, 20.0)
                     
-                    Button(action: {
-                        // TODO: set genderType to woman
-                        store.send(.setGender(.woman))
-                    }, label: {
-                        Images.genderWoman.swiftUIImage
-                            .resizable()
-                            .frame(width: 160, height: 160)
-                            .clipShape(.circle)
-                            .opacity(store.gender == .woman ? 1.0 : 0.6)
-                    })
+                    HStack {
+                        Button(action: {
+                            // TODO: set genderType to man
+                            store.send(.setGender(.man))
+                        }, label: {
+                            Images.genderMan.swiftUIImage
+                                .resizable()
+                                .frame(width: 160, height: 160)
+                                .clipShape(.circle)
+                                .opacity(store.gender == .man ? 1.0 : 0.6)
+                        })
+                        
+                        Button(action: {
+                            // TODO: set genderType to woman
+                            store.send(.setGender(.woman))
+                        }, label: {
+                            Images.genderWoman.swiftUIImage
+                                .resizable()
+                                .frame(width: 160, height: 160)
+                                .clipShape(.circle)
+                                .opacity(store.gender == .woman ? 1.0 : 0.6)
+                        })
+                    }
+                    .padding(.top, 80.0)
+                    .padding(.horizontal)
+                    
+                    Spacer()
                 }
-                .padding(.top, 80.0)
-                .padding(.horizontal)
-                
-                Spacer()
+            } destination: { store in
+                NicknameInputView(store: store)
             }
-        } destination: { store in
-            NicknameInputView(store: store)
         }
     }
 }
