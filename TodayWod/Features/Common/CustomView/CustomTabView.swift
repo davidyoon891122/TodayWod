84c0ec77b328285d1adca8fb6632f84c0b12ee75
David Yoon <qqpo12@naver.com>
2024-09-18 16:49:21 +0900

    [TOW-37] 테스트 뷰 제거 및 아이템 분리
    * 탭 아이템 분리
    * 테스트 뷰 삭제

--- TodayWod/Features/Common/CustomView/CustomTabView.swift
+++ TodayWod/Features/Common/CustomView/CustomTabView.swift
@@ -7,30 +7,6 @@
 
 import SwiftUI
 
-enum TabMenuItem: CaseIterable {
-    case home
-    case settings
-
-    var selectedImage: Image {
-        switch self {
-        case .home:
-            Images.icHomeActivate.swiftUIImage
-        case .settings:
-            Images.icAccountActivate.swiftUIImage
-        }
-    }
-
-    var deactivatedImage: Image {
-        switch self {
-        case .home:
-            Images.icHomeDisable.swiftUIImage
-        case .settings:
-            Images.icAccountDisable.swiftUIImage
-        }
-    }
-
-}
-
 struct CustomTabView: View {
 
     @Binding var selectedItem: TabMenuItem
