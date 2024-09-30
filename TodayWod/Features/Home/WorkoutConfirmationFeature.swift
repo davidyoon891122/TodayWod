5a50bd9da8225b97562816147058ab7d03106687
davidyoon891122 <37206715+davidyoon891122@users.noreply.github.com>
2024-09-30 15:02:55 +0900

    [TOW-56] 운동 완료 확인 화면 개발 (#32)
    * 운동 완료 확인 화면 개발
    
    Co-authored-by: David Yoon <davidyoon@hanwha.com>

--- TodayWod/Features/Home/WorkoutConfirmationFeature.swift
+++ TodayWod/Features/Home/WorkoutConfirmationFeature.swift
@@ -1,67 +1,60 @@
 //
-//  CelebrateFeature.swift
+//  WorkoutConfirmationFeature.swift
 //  TodayWod
 //
-//  Created by Jiwon Yoon on 9/25/24.
+//  Created by Davidyoon on 9/30/24.
 //
 
 import Foundation
 import ComposableArchitecture
 
 @Reducer
-struct CelebrateFeature {
-
+struct WorkoutConfirmationFeature {
+    
     @ObservableState
     struct State: Equatable {
-
+        
     }
-
+    
     enum Action {
-        case didTapCloesButton
+        case didTapCloseButton
     }
-
+    
     @Dependency(\.dismiss) var dismiss
-
+    
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
-            case .didTapCloesButton:
-
+            case .didTapCloseButton:
                 return .run { _ in await dismiss() }
             }
         }
     }
-
+    
 }
 
 import SwiftUI
 
-struct CelebrateView: View {
-
-    let store: StoreOf<CelebrateFeature>
-
+struct WorkoutConfirmationView: View {
+    
+    let store: StoreOf<WorkoutConfirmationFeature>
+    
     var body: some View {
-        VStack {
-            Images.trophy.swiftUIImage
-                .resizable()
-                .frame(width: 120.0, height: 120.0)
-
-            Text("승리의 순간이에요!")
+        VStack(alignment: .leading) {
+            Text("운동을 완료할까요?")
                 .font(Fonts.Pretendard.bold.swiftUIFont(size: 24.0))
                 .foregroundStyle(.grey100)
-                .padding(.top, 20.0)
-
-            Text("6일간의 여정을 모두 정복했어요.\n이제 어떤 도전도 당신을 막을 수 없어요!")
+            
+            Text("아직 완료하지 않은 운동이 있어요.\n체크한 운동만 기록돼요.")
                 .font(Fonts.Pretendard.regular.swiftUIFont(size: 16.0))
                 .foregroundStyle(.grey80)
-                .multilineTextAlignment(.center)
                 .padding(.top, 10.0)
-
+            
             HStack {
                 Button(action: {
-                    store.send(.didTapCloesButton)
+                    store.send(.didTapCloseButton)
                 }, label: {
-                    Text("닫기")
+                    Text("취소")
                         .font(Fonts.Pretendard.bold.swiftUIFont(size: 16.0))
                         .foregroundStyle(.grey100)
                         .frame(maxWidth: .infinity, minHeight: 56.0)
@@ -72,13 +65,12 @@ struct CelebrateView: View {
                 Button(action: {
                     // TODO: - did tap new challenge
                 }, label: {
-                    Text("새로운 도전하기")
+                    Text("운동 완료")
                         .font(Fonts.Pretendard.bold.swiftUIFont(size: 16.0))
                         .foregroundStyle(.white)
                         .frame(maxWidth: .infinity, minHeight: 56.0)
                         .background(.blue60)
                         .clipShape(.rect(cornerRadius: 300.0))
-
                 })
             }
             .padding(.top, 40.0)
@@ -88,12 +80,12 @@ struct CelebrateView: View {
         .padding(.bottom, 20.0)
         .background(.white)
     }
-
+    
 }
 
 #Preview {
-    CelebrateView(store: Store(initialState: CelebrateFeature.State()) {
-        CelebrateFeature()
+    WorkoutConfirmationView(store: Store(initialState: WorkoutConfirmationFeature.State()) {
+        WorkoutConfirmationFeature()
     })
-    .border(.red100)
 }
+
--- TodayWod/Features/Settings/MyActivityFeature.swift
+++ TodayWod/Features/Settings/MyActivityFeature.swift
@@ -115,7 +115,6 @@ struct MyActivityView: View {
                 MyPageView(store: store)
             }
         }
-
     }
 
 }
