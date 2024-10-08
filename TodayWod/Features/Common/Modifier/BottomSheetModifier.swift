75035b1d31891c30f3efc2da52edb364fd7eda04
David Yoon <davidyoon@hanwha.com>
2024-10-08 10:21:38 +0900

    [TOW-41] 바텀쉬트 모디파이어 리팩터링
    * 동적으로 sheet 뷰 높이 계산해서 snapDistance 처리 하도록 수정

--- TodayWod/Features/Common/Modifier/BottomSheetModifier.swift
+++ TodayWod/Features/Common/Modifier/BottomSheetModifier.swift
@@ -14,35 +14,43 @@ struct BottomSheetModifier<Item: Identifiable, SheetContent: View>: ViewModifier
     let sheetContent: (Item) -> SheetContent
 
     @GestureState private var translation: CGFloat = 0
+    @State private var sheetHeight: CGFloat = 0
 
     func body(content: Content) -> some View {
         ZStack {
             content
             if let item = item {
                 ZStack(alignment: .bottom) {
-                    VStack {
-                        sheetContent(item)
-                            .transition(.move(edge: .bottom))
-                            .cornerRadius(16, corners: [.topLeft, .topRight])
-                            .shadow(color: Color.gray.opacity(0.3), radius: 10)
-                            .offset(y: max(self.translation, 0))
-                            .gesture(
-                                DragGesture()
-                                    .updating(self.$translation) { value, state, _ in
-                                        state = value.translation.height
+                    
+                    sheetContent(item)
+                        .background(
+                            GeometryReader { geometry in
+                                Color.clear
+                                    .onAppear {
+                                        self.sheetHeight = geometry.size.height
                                     }
-                                    .onEnded { value in
-                                        let snapDistance = 200 * 0.25 // Adjust as needed
-                                        guard abs(value.translation.height) > snapDistance else {
-                                            return
-                                        }
-                                        withAnimation {
-                                            self.item = nil
-                                            self.onDismiss?()
-                                        }
+                            }
+                        )
+                        .transition(.move(edge: .bottom))
+                        .cornerRadius(16, corners: [.topLeft, .topRight])
+                        .shadow(color: Color.gray.opacity(0.3), radius: 10)
+                        .offset(y: max(self.translation, 0))
+                        .gesture(
+                            DragGesture()
+                                .updating(self.$translation) { value, state, _ in
+                                    state = value.translation.height
+                                }
+                                .onEnded { value in
+                                    let snapDistance = self.sheetHeight * 0.25 // Adjust as needed
+                                    guard abs(value.translation.height) > snapDistance else {
+                                        return
                                     }
-                            )
-                    }
+                                    withAnimation {
+                                        self.item = nil
+                                        self.onDismiss?()
+                                    }
+                                }
+                        )
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
                 .ignoresSafeArea()
