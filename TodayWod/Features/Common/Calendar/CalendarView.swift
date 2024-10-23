3feb479920e0fc6c8454572bb7b895153665e78c
jiyeonO <jiyeonO>
2024-10-24 08:47:25 +0900

    리뷰 반영 2

--- TodayWod/Features/Common/Calendar/CalendarView.swift
+++ TodayWod/Features/Common/Calendar/CalendarView.swift
@@ -9,29 +9,16 @@ import SwiftUI
 
 struct CalendarView: View {
     
-    @State var month: Date
-    @Binding var markedDates: Set<Date>
     @State private var offset: CGSize = CGSize()
+    
+    var month: Date
+    @Binding var markedDates: Set<Date>
     
     var body: some View {
         VStack {
             CalendarHeaderView(month: month)
             CalendarGridView(month: month, markedDates: $markedDates)
         }
-        .gesture(
-            DragGesture()
-                .onChanged { gesture in
-                    self.offset = gesture.translation
-                }
-                .onEnded { gesture in
-                    if gesture.translation.width < -100 {
-                        month = month.changeMonth(by: 1)
-                    } else if gesture.translation.width > 100 {
-                        month = month.changeMonth(by: -1)
-                    }
-                    self.offset = CGSize()
-                }
-        )
     }
 
 }
