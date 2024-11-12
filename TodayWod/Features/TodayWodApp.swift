ed520c45d62bd14cc0eb953dab28723bc9a94013
jiyeonO <first3512@naver.com>
2024-11-12 15:18:23 +0900

    [TOW-228][iOS][Firebase] Firebase 프로젝트 생성 및 적용 (#116)
    * [TOW-228] apply Firebase Crashlytics
    
    * add Google Analytics
    
    ---------
    
    Co-authored-by: jiyeonO <jiyeonO>

--- TodayWod/Features/TodayWodApp.swift
+++ TodayWod/Features/TodayWodApp.swift
@@ -7,10 +7,24 @@
 
 import SwiftUI
 import ComposableArchitecture
+import Firebase
+import FirebaseCore
+import FirebaseAnalytics
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
 
 @main
 struct TodayWodApp: App {
 
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     static let store = Store(initialState: TodayWod.State.splash(SplashFeature.State())) {
         TodayWod()
     }
