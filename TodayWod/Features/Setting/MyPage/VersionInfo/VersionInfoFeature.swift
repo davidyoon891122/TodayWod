01ba310d352bb69004d13bfa2ac550edd5454b11
David Yoon <qqpo12@naver.com>
2024-11-23 22:15:31 +0900

    [TOW-272] 버전 확인 파라미터 추가
    * 앱 스토어 특정 국가 대상의 앱의 country 파라미터 필요하여 값 추가 처리

--- TodayWod/Features/Setting/MyPage/VersionInfo/VersionInfoFeature.swift
+++ TodayWod/Features/Setting/MyPage/VersionInfo/VersionInfoFeature.swift
@@ -30,11 +30,11 @@ struct VersionInfoFeature {
         Reduce { state, action in
             switch action {
             case .onAppear:
-                let bundleId = PlistReader().identifier // "com.ycompany.DreamTodo"
+                let bundleId = PlistReader().identifier
                 return .run { send in
                     do {
                         let result = try await apiClient.requestAppVersion(.init(bundleId: bundleId))
-                        
+
                         await send(.versionRequestResult(.success(result)))
                     } catch {
                         await send(.versionRequestResult(.failure(error)))
--- TodayWod/Repository/Model/AppStoreResultRequestModel.swift
+++ TodayWod/Repository/Model/AppStoreResultRequestModel.swift
@@ -10,5 +10,11 @@ import Foundation
 struct AppStoreResultRequestModel: Codable, RequestBodyPresentable {
     
     let bundleId: String
-    
+    let country: String
+
+    init(bundleId: String, country: String = "kr") {
+        self.bundleId = bundleId
+        self.country = country
+    }
+
 }
