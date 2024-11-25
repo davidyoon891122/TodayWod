1af591d3d869e214715d0dde81bc8aa2ea57d9ee
David Yoon <qqpo12@naver.com>
2024-11-25 21:49:58 +0900

    [TOW-280] 버튼 disabled 처리
    * Throttle 처리 안되어 isProcessing 변수 추가하여, 버튼 disable 처리로 변경

--- TodayWod/Features/Onboarding/Gender/GenderSelectFeature.swift
+++ TodayWod/Features/Onboarding/Gender/GenderSelectFeature.swift
@@ -27,17 +27,15 @@ struct GenderSelectFeature {
         var gender: GenderType? = nil
         var path = StackState<Path.State>()
         var onboardingUserModel: OnboardingUserInfoModel = .init()
+        var isProcessing: Bool = false
     }
     
     enum Action {
+        case onAppear
         case setGender(GenderType)
         case path(StackActionOf<Path>)
         case toNickname
         case finishOnboarding
-    }
-
-    enum ThrottleID: Hashable {
-        case throttle
     }
 
     @Dependency(\.continuousClock) var clock
@@ -45,7 +43,13 @@ struct GenderSelectFeature {
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
+            case .onAppear:
+                state.isProcessing = false
+                return .none
             case let .setGender(genderType):
+                guard !state.isProcessing else { return .none }
+
+                state.isProcessing = true
                 state.gender = genderType
                 state.onboardingUserModel.gender = genderType
                 let generator = UIImpactFeedbackGenerator(style: .medium)
@@ -53,12 +57,6 @@ struct GenderSelectFeature {
                 generator.impactOccurred()
 
                 return .send(.toNickname)
-                    .throttle(
-                        id: ThrottleID.throttle,
-                        for: 0.5,
-                        scheduler: DispatchQueue.main,
-                        latest: false
-                    )
             case let .path(action):
                 switch action {
                 case .element(id: _, action: .nickName(.finishInputNickname(let heightState))):
@@ -162,7 +160,8 @@ struct GenderSelectView: View {
                                     .clipShape(.circle)
                                     .opacity(store.gender == .man ? 1.0 : 0.6)
                             })
-                            
+                            .disabled(store.isProcessing)
+
                             Button(action: {
                                 store.send(.setGender(.woman))
                             }, label: {
@@ -172,10 +171,14 @@ struct GenderSelectView: View {
                                     .clipShape(.circle)
                                     .opacity(store.gender == .woman ? 1.0 : 0.6)
                             })
+                            .disabled(store.isProcessing)
                         }
                         .padding(.top, 80.0)
                         .padding(.horizontal)
                     }
+                }
+                .onAppear {
+                    store.send(.onAppear)
                 }
             } destination: { store in
                 WithPerceptionTracking {
